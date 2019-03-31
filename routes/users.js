9162cfc71f085388e13c3eb31d88becf0ada7deb
Pankaj Gupta <pankaj.gupta@teamlance.io>
2019-04-01 00:34:49 +0530

    jwt token in output of login api

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../schemas/userSchema');
+const jwt = require('jsonwebtoken');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/addUser/', (req, res, next) => {  
+router.post('/addUser/', (req, res, next) => {
   console.log('resdaffsdafsd ------------', req.body);
   var newUser = new User({
     name: req.body.name,
@@ -15,8 +16,8 @@ router.post('/addUser/', (req, res, next) => {
     password: req.body.password,
   })
 
-  newUser.save(function(err, user) {
-    if(err) {
+  newUser.save(function (err, user) {
+    if (err) {
       next(err)
       res.statusMessage(500)
       res.render('error', { error: err })
@@ -29,9 +30,14 @@ router.post('/addUser/', (req, res, next) => {
 
 
 router.post('/login', (req, res, next) => {
-  User.findOne({username: req.body.username, password: req.body.password}, function(err, user) {
-    if(err) throw err;
-    res.send(user)
+  User.findOne({ username: req.body.username, password: req.body.password }, function (err, user) {
+    if (err) throw err;
+    var token = jwt.sign({ userId: user._id }, 'mytokenkey');
+    // res.send(user)
+    res.status(200).json({
+      // ...user,
+      token
+    })
   })
 })
 
